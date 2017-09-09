a2a6c6ecec0294af27ea2d456fbc87a4da77f0f2
nic-maharjan <mhrjnn.nickeshh@gmail.com>
2017-09-09 13:58:16 -0700

    added new variables

--- src/core_java/HelloGit.java
+++ src/core_java/HelloGit.java
@@ -3,7 +3,8 @@ package core_java;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Learning Git basics");
+		String username = "Nickesh";
+		System.out.println(username + " learning Git basics");
 	}
 
 }
